7d200d101287c22056e7253361c95e6d4c39282a
yoshiaki.yoshida <yoshiaki.yoshida@techacademy.jp>
2020-01-18 10:49:22 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.page(params[:page]).per(25)
+    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    counts(@user)
   end
 
   def new
@@ -23,9 +26,21 @@ class UsersController < ApplicationController
       render :new
     end
   end
- 
+
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+
   private
- 
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
