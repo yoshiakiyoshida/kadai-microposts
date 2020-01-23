c8fc4a5ee7d5cfaf124bd96b5ebb62b193d18636
yoshiaki.yoshida <yoshiaki.yoshida@techacademy.jp>
2020-01-23 14:26:35 +0000

    favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ApplicationRecord
     has_many :followings, through: :relationships, source: :follow
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
+    has_many :favorites
+    has_many :microposts, through: :favorites
+
 
   def follow(other_user)
     unless self == other_user
